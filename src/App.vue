b0c8068618a16d476ab1f29d0e17cab9a5600fe3
Chainlayer | Peter <peter@chainlayer.io>
2019-07-30 13:56:54 +0200

    Add warning about Windows 10

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app" style="vertical-align:top">
-    <h3>Ledger delegation tool ChainLayer</h3>
+    <h3>ChainLayer Delegation Tool</h3>
+    <hr>
     <div class="alert alert-warning" v-if="this.$browserDetect.meta.name!='Chrome'">Delegating with Ledger only supported on Chrome</div>
     <CosmosLedger msg="Delegation"/>
     <TerraLedger msg="Delegation"/>
     <IrisLedger msg="Delegation"/>
+    <hr>
+    <div class="alert alert-warning">Delegating on Windows 10 with a Ledger Nano S has had issues since the last Windows update. See <a href="https://www.ledger.com/your-ledger-device-and-the-upcoming-windows-10-update/" rel="nofollow noreferrer noopener">this Ledger post</a> for details</div>
   </div>
 </template>
 
